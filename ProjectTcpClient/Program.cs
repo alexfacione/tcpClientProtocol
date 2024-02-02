3ab82846055d1deda9d0780ffac99c573b2f4db4
Alexandre Oliveira Facione <alexfacione@yahoo.com.br>
2024-02-02 19:13:23 -0300

    feature: adds the project

---
+++ ProjectTcpClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+/// <summary>
+/// <title> TCP CLIENT</title>
+/// Author: Alexandre de Oliveira Facione
+/// Description: TCP Client developed in .NET 6
+/// </summary>
+
+const string ipAddress = "127.0.0.1";
+const int portTcpServer = 3124;
+
+// TCP CLIENT
+TcpClient client = new TcpClient(ipAddress, portTcpServer);
+Console.WriteLine($"CLIENT: Conected to server {ipAddress}:{portTcpServer}");
+
+NetworkStream stream = client.GetStream();
+
+Console.WriteLine($"\nPress <E> to finish");
+var message = String.Empty;
+do
+{
+    Console.WriteLine($"\nSend your message to server: ");
+    message = Console.ReadLine() ?? "";
+
+    if (message.Trim().Equals("e", StringComparison.InvariantCultureIgnoreCase))
+        break;
+
+    // Send message to server
+    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+    stream.Write(messageBytes, 0, messageBytes.Length);
+    Console.WriteLine($"Message sent: {message}");
+
+    // Response form server
+    byte[] responseBuffer = new byte[4096];
+    int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+    string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+    Console.WriteLine($"Response from server: {response}");
+
+} while (true);
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# tcpClientProtocol
-Example of a C# application (Client) communicating via the TCP protocol
+# TCP Client Protocol
+
+## Version
+TCP Client developed:
+* C# 10
+* .NET 6
+
+## Description
+Example of a C# application (Client) communicating via the TCP protocol. By default, the application is listening on port 3124. This is same port of TCP Server project repository.
+
+## Step
+#### Execute the aplications:
+* First: Execute the TCP Server application
+ https://github.com/alexfacione/tcpServerProtocol
+* Second: Execute the TCP Client application
+
+#### Note:
+The constant "ipAddress" is the IP of PC running the TCP Server application.
+
+The constant "portTcpServer" is the port of PC running the TCP Server application.
